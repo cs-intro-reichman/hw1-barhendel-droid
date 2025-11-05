369b752783b4e124fa1a8a76572a431875560928
barhendel-droid <barhendel@gmail.com>
2025-11-05 21:03:31 +0200

    Your commit message

--- Demo0.java
+++ Demo0.java
@@ -1,9 +1,24 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
-public class PrintTo5 {
+public class Demo0 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
         // Complete the program's code here:
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("done");
+
     }
  }
+ 
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,11 @@
-// Prints a given number using a hundreds, tens, and units notation.
+// Generates three integer random numbers in a given range,
+// and prints them in increasing order.
 public class NumWords {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+	public static void main(String[] args) {
+		int num1 = Integer.parseInt(args [0]);
+		int ones = num1 % 10;
+		int tens = (num1/10)%10;
+		int hundreds = num1/100;
+		System.out.println( "ones" + ones + "tens" +  tens + "hundreds" + hundreds);
 	}
 }
